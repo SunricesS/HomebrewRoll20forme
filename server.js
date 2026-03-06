56f892f54bb5c42ea82d12c2bb7660a94c3f6fae
Güneş Pirinçoğlu <gp01prc@gmail.com>
2026-03-06 23:34:41 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -177,6 +177,8 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('Sunucu 3000 portunda aktif: http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Sunucu ${PORT} portunda aktif!`);
 });
